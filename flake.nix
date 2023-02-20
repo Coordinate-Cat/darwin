8bdfab652090a8f6cbff9ee92bbedf550586f4da
Coordinate-Cat <tetrapasta02@gmail.com>
2023-02-20 17:14:03 +0900

    feat: flake

--- flake.nix
+++ flake.nix
@@ -21,11 +21,14 @@
           }
         ];
         # Home-Manager を有効にする
-        home-manager.use = true;
-        # Home-Manager のバージョンを指定する
-        home-manager.version = "21.05";
-        # Home-Manager の設定を有効にする
-        home-manager.enable = true;
+        home-manager = nixpkgs.legacyPackages.${system}.home-manager.override {
+          # Home-Manager のバージョンを指定する
+          version = "21.05";
+          # Home-Manager の設定を有効にする
+          enable = true;
+          # Home-Manager の設定ファイルのパスを指定する
+          home.file = [ { source = /Users/my-user/.config/nixpkgs/home.nix; destination = ".config/nixpkgs/home.nix"; } ];
+        };
       };
     };
   };
