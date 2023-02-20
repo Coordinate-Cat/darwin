df084a29970300ec4e74b009df019e7dfedcc372
Coordinate-Cat <tetrapasta02@gmail.com>
2023-02-20 17:19:08 +0900

    feat: flake

--- flake.nix
+++ flake.nix
@@ -7,29 +7,35 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    nixConfigurations = {
-      my-config = nixpkgs.lib.nixosSystem {
-        system = "darwin-arm64";
-        modules = [
-          {
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "darwin-arm64";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      nixConfigurations = {
+        my-config = pkgs.lib.nixosSystem {
+          system = "darwin-arm64";
+          modules = [
+            {
+              # Home-Manager の設定ファイルのパスを指定する
+              home-manager.users.my-user = {
+                # ~/.config/nixpkgs/home.nix に記述した設定ファイルのパスを指定する
+                home.file = [ { source = /Users/my-user/.config/nixpkgs/home.nix; destination = ".config/nixpkgs/home.nix"; } ];
+              };
+            }
+          ];
+          # Home-Manager を有効にする
+          home-manager = pkgs.legacyPackages.${system}.home-manager.override {
+            # Home-Manager のバージョンを指定する
+            version = "21.05";
+            # Home-Manager の設定を有効にする
+            enable = true;
             # Home-Manager の設定ファイルのパスを指定する
-            home-manager.users.my-user = {
-              # ~/.config/nixpkgs/home.nix に記述した設定ファイルのパスを指定する
-              home.file = [ { source = /Users/my-user/.config/nixpkgs/home.nix; destination = ".config/nixpkgs/home.nix"; } ];
-            };
-          }
-        ];
-        # Home-Manager を有効にする
-        home-manager = nixpkgs.legacyPackages.${system}.home-manager.override {
-          # Home-Manager のバージョンを指定する
-          version = "21.05";
-          # Home-Manager の設定を有効にする
-          enable = true;
-          # Home-Manager の設定ファイルのパスを指定する
-          home.file = [ { source = /Users/my-user/.config/nixpkgs/home.nix; destination = ".config/nixpkgs/home.nix"; } ];
+            home.file = [ { source = /Users/my-user/.config/nixpkgs/home.nix; destination = ".config/nixpkgs/home.nix"; } ];
+          };
         };
       };
     };
-  };
 }
