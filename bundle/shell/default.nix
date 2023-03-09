191ecaf14291bbf987023694689047f6b63ebf84
Coordinate-Cat <tetrapasta02@gmail.com>
2023-03-09 15:56:20 +0900

    feat: shell

--- bundle/shell/default.nix
+++ bundle/shell/default.nix
@@ -111,6 +111,14 @@ in
       source "$HOME/.cargo/env"
 
       [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
+
+      # expo
+      source ~/`[[ $SHELL == *"zsh" ]] && echo '.zshenv.d' || echo '.bash_profile'`
+    '';
+    envExtra = ''
+      [ -d "$HOME/Library/Android/sdk" ] && ANDROID_HOME=$HOME/Library/Android/sdk || ANDROID_HOME=$HOME/Android/Sdk
+      echo "export ANDROID_HOME=$ANDROID_HOME" >> ~/`[[ $SHELL == *"zsh" ]] && echo '.zshenv.d' || echo '.bash_profile'`
+      echo "export PATH=$ANDROID_HOME/platform-tools:\$PATH" >> ~/`[[ $SHELL == *"zsh" ]] && echo '.zshenv.d' || echo '.bash_profile'`
     '';
   };
 }
